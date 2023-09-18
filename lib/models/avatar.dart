4b1d5fb6ac2a96f5ce80b1474902e7216602fc24
stefanoperico <stefano.perico@inter-invest.fr>
2023-09-18 09:25:54 +0200

    add method to get avatars

--- lib/models/avatar.dart
+++ lib/models/avatar.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+import 'package:path_provider/path_provider.dart';
 import 'package:uuid/uuid.dart';
 
 final class Avatar {
@@ -9,4 +11,25 @@ final class Avatar {
     required this.name,
     required this.path,
   }) : id = const Uuid().v4();
+
+  Future<List<Avatar>> getAvatars() async {
+    final directory = await getApplicationDocumentsDirectory();
+    final avatarDirectory = Directory('${directory.path}/avatars');
+
+    if (!await avatarDirectory.exists()) {
+      return [];
+    }
+
+    final avatarFiles = await avatarDirectory.list().toList();
+    final avatars = <Avatar>[];
+
+    for (final avatarFile in avatarFiles) {
+      if (avatarFile is File) {
+        avatars.add(Avatar(
+            name: avatarFile.uri.pathSegments.last, path: avatarFile.path));
+      }
+    }
+
+    return avatars;
+  }
 }
